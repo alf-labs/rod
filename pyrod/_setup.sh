8382a85c3a9c5ad2733cf8223bd416c626addc77
ralf <ralfoide@gmail.com>
2026-02-28 16:25:41 -0800

    PyRod: Adjust setup for Cygwin.

--- pyrod/_setup.sh
+++ pyrod/_setup.sh
@@ -5,15 +5,23 @@ DIR=venv_rod
 cd $(dirname "$0")
 pwd
 
-if [[ ! -d $DIR ]]; then
-    set -x
-    python -m venv --system-site-packages $DIR
-    source $DIR/bin/activate
-    $DIR/bin/pip install opencv-python numpy imutils flask
-    if [[ $(uname -r) =~ CYGWIN ]]; then
-        # TBD echo "Cygwin stuff here"
-    fi
+PY="python"
+
+if [[ $(uname -s) =~ CYGWIN ]]; then
+    # The current numpy/cv2 requires a Python >= 3.7 and <= 3.11.
+    # Cygwin currently offers 3.9 and 3.12. Fails with the latter.
+    PY="python3.9"
 fi
 
-
-
+if [[ ! -d $DIR ]]; then
+    set -x
+    $PY -m venv --system-site-packages $DIR
+    source $DIR/bin/activate
+    if [[ $(uname -s) =~ CYGWIN ]]; then
+        echo "WARNING: Building opencv-python under Cygwin takes forever."
+        $DIR/bin/pip install numpy imutils flask
+        $DIR/bin/pip install --verbose opencv-python
+    else
+        $DIR/bin/pip install opencv-python numpy imutils flask
+    fi
+fi
--- pyrod/pyrod.py
+++ pyrod/pyrod.py
@@ -14,14 +14,20 @@ try:
     import numpy as np
     import imutils
     from flask import Flask, render_template, Response, request, jsonify
-    from ultralytics import YOLO
 except ModuleNotFoundError as e:
     print(f"ERROR: Missing library. {e}")
-    print( "To fix: $ pip install opencv-python-headless numpy imutils flask ultralytics")
+    print( "To fix: $ pip install opencv-python numpy imutils flask")
     print(f"or    : $ python {sys.argv[0]}")
     exit(1)
 
+WINDOW_TITLE = "Rod Sample"
+VIDEOS = [
+    "../samples/rod1_front_randall_up_2025-03-23.mp4",
+    "../samples/rod1_rear_randall_up_2025-03-23.mp4",
+]
 
+FPS = 30
+FPS_MS = 1000//FPS
 
 class Main:
     def __init__(self):
@@ -30,6 +36,20 @@ class Main:
     def run(self):
         print("@@ Run")
 
+        cap = cv2.VideoCapture(VIDEOS[0])
+        try:
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                cv2.imshow(WINDOW_TITLE, frame)
+                if cv2.waitKey(FPS_MS) & 0xFF == ord('q'):
+                    break
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()
+
+        print("@@ end")
 
 if __name__ == "__main__":
     m = Main()
