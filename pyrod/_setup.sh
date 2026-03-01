19576b36a968b8606d396af95ff9030d9a72dfa9
ralf <ralfoide@gmail.com>
2026-02-28 16:43:58 -0800

    PyRod: Adjust setup for MingW.

--- pyrod/_setup.sh
+++ pyrod/_setup.sh
@@ -7,20 +7,29 @@ pwd
 
 PY="python"
 
-if [[ $(uname -s) =~ CYGWIN ]]; then
+if [[ $(uname -s) =~ CYGWIN_NT ]]; then
     # The current numpy/cv2 requires a Python >= 3.7 and <= 3.11.
     # Cygwin currently offers 3.9 and 3.12. Fails with the latter.
     PY="python3.9"
+elif [[ $(uname -s) =~ MINGW64_NT ]]; then
+    # This is either Git Bash or MingW.
+    if [[ ! $("$PY" --version) ]]; then
+        PPY=$(cygpath "$LOCALAPPDATA\Programs\Python\Python3")
+        PY="$PPY/python.exe"
+    fi
 fi
+
+echo -n "Running: " ; "$PY" --version
+exit 1
 
 if [[ ! -d $DIR ]]; then
     set -x
-    $PY -m venv --system-site-packages $DIR
+    "$PY" -m venv --system-site-packages $DIR
     source $DIR/bin/activate
-    if [[ $(uname -s) =~ CYGWIN ]]; then
+    if [[ $(uname -s) =~ CYGWIN ||  $(uname -s) =~ MINGW64_NT ]]; then
         echo "WARNING: Building opencv-python under Cygwin takes forever."
         $DIR/bin/pip install numpy imutils flask
-        $DIR/bin/pip install --verbose opencv-python
+        nice $DIR/bin/pip install --verbose opencv-python
     else
         $DIR/bin/pip install opencv-python numpy imutils flask
     fi
