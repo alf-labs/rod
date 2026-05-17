3449e4de5677a3461fced671944943e6a72f37ee
ralf <ralfoide@gmail.com>
2026-05-17 12:23:40 -0700

    PyRod1+2: Samples.

--- pyrod1/_test.sh
+++ pyrod1/_test.sh
@@ -8,7 +8,8 @@ ARGS="--display=full --overlay-video --locator-rod-sz=30,15,50,/1280"
 if [[ "$A" == "0p0" ]]; then
     mkdir -p output
     ( set -x
-    time ./_run.sh -i 0 --start 0:20 --end 0:45 -0 $ARGS $@
+    # time ./_run.sh -i 0 --start 0:20 --end 0:45 -0 $ARGS $@
+    time ./_run.sh -i 0 -0 $ARGS $@
     )
 elif [[ "$A" == "0p1" ]]; then
     ( set -x
